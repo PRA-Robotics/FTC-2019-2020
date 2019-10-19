797bf588413a469f860b96a2a114d18740460333
Jackson Lucchesi <jacheesy@gmail.com>
2019-10-19 11:39:44 -0600

    added dual rev hub support and an Intake class

---
+++ Intake.java
@@ -0,0 +1,24 @@
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class Intake {
+    private double power;
+
+    Intake() {
+        power = 1.0;
+    }
+
+    public void run(DcMotor a, DcMotor b) {
+        a.setPower(power);
+        b.setPower(power);
+        //irDrive.setPower(power);
+        //ilDrive.setPower(power);
+    }
+
+    public double getPower() {
+        return this.power;
+    }
+
+    public void setPower(double power) {
+        this.power = power;
+    }
+}
--- RemoteControl.java
+++ RemoteControl.java
@@ -3,32 +3,60 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name="Remote Control", group="TeleOp")
-public class RemoteControl extends OpMode{
+public class RemoteControl extends OpMode {
+  //public DcMotor[] motors = DcMotor[8];
   public DcMotor flDrive;
   public DcMotor frDrive;
   public DcMotor blDrive;
   public DcMotor brDrive;
+  public DcMotor irDrive;
+  public DcMotor ilDrive;
+  final double IntakePower = 1.0;
+
+  //public Intake greenIn = new Intake();
+
   public void init(){
+      /*DcMotor[0] = hardwareMap.get(DcMotor.class, "flDrive");
+      DcMotor[1] = hardwareMap.get(DcMotor.class, "frDrive");
+      DcMotor[2] = hardwareMap.get(DcMotor.class, "blDrive");
+      DcMotor[3] = hardwareMap.get(DcMotor.class, "brDrive");
+      DcMotor[4] = hardwareMap.get(DcMotor.class, "ilDrive");
+      DcMotor[5] = hardwareMap.get(DcMotor.class, "irDrive");*/
     flDrive = hardwareMap.get(DcMotor.class, "flDrive");
     frDrive = hardwareMap.get(DcMotor.class, "frDrive");
     blDrive = hardwareMap.get(DcMotor.class, "blDrive");
     brDrive = hardwareMap.get(DcMotor.class, "brDrive");
+    irDrive = hardwareMap.get(DcMotor.class, "irDrive");
+    ilDrive = hardwareMap.get(DcMotor.class, "ilDrive");
+    blDrive.setDirection(DcMotor.Direction.REVERSE);
+    irDrive.setDirection(DcMotor.Direction.REVERSE);
   }
 
   public void loop(){
-    flDrive.setPower((-Math.sqrt(2) * gamepad1.left_stick_y / 2 + Math.sqrt(2) * gamepad1.left_stick_x / 2) * Math.min(1 , 1 + gamepad1.right_stick_x));
+    /*motors[0].setPower((Math.sqrt(2) * gamepad1.left_stick_y / 2 - Math.sqrt(2) * gamepad1.left_stick_x / 2) * Math.min(1 , 1 + gamepad1.right_stick_x));
+    motors[1].setPower((Math.sqrt(2) * gamepad1.left_stick_y / 2 - Math.sqrt(2) * gamepad1.left_stick_x / 2) * Math.min(1 , 1 + gamepad1.right_stick_x));
+    motors[2].setPower((Math.sqrt(2) * gamepad1.left_stick_y / 2 - Math.sqrt(2) * gamepad1.left_stick_x / 2) * Math.min(1 , 1 + gamepad1.right_stick_x));
+    motors[3].setPower((Math.sqrt(2) * gamepad1.left_stick_y / 2 - Math.sqrt(2) * gamepad1.left_stick_x / 2) * Math.min(1 , 1 + gamepad1.right_stick_x));*/
+    flDrive.setPower((Math.sqrt(2) * gamepad1.left_stick_y / 2 - Math.sqrt(2) * gamepad1.left_stick_x / 2) * Math.min(1 , 1 + gamepad1.right_stick_x));
     frDrive.setPower((Math.sqrt(2) * gamepad1.left_stick_y / 2 + Math.sqrt(2) * gamepad1.left_stick_x / 2) * Math.min(1 , 1 - gamepad1.right_stick_x));
     blDrive.setPower((Math.sqrt(2) * gamepad1.left_stick_y / 2 + Math.sqrt(2) * gamepad1.left_stick_x / 2)  * Math.min(1 , 1 + gamepad1.right_stick_x));
     brDrive.setPower((Math.sqrt(2) * gamepad1.left_stick_y / 2 - Math.sqrt(2) * gamepad1.left_stick_x / 2) * Math.min(1 , 1 - gamepad1.right_stick_x));
-    if(Math.pow(gamepad1.left_stick_y, 2) + Math.pow(gamepad1.left_stick_x, 2)){
-      flDrive.setPower(gamepad1.right_stick_x);
-      frDrive.setPower(-gamepad1.right_stick_x);
-      blDrive.setPower(gamepad1.right_stick_x);
-      brDrive.setPower(-gamepad1.right_stick_x);
+
+    /*while (gamepad1.right_bumper) {
+        irDrive.setPower(IntakePower);
+        //Telemetry.addData("yooo");
+        ilDrive.setPower(IntakePower);
+    }*/
+
+    if(Math.pow(gamepad1.left_stick_y, 2) + Math.pow(gamepad1.left_stick_x, 2) == 0){
+      /*motors[0].setPower(-gamepad1.right_stick_x*.6);
+      motors[1].setPower(-gamepad1.right_stick_x*.6);
+      motors[2].setPower(-gamepad1.right_stick_x*.6);
+      motors[3].setPower(-gamepad1.right_stick_x*.6);*/
+      flDrive.setPower(-gamepad1.right_stick_x*.6);
+      frDrive.setPower(gamepad1.right_stick_x*.6);
+      blDrive.setPower(-gamepad1.right_stick_x*.6);
+      brDrive.setPower(gamepad1.right_stick_x*.6);
     }
-  }
-
-  public void move(double x, double y){//X and Y are local positioning based of current rotation
-
   }
 }
