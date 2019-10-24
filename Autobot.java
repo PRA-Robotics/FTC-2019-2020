2171924357982be4bf5cb6c37a30f1f3dbb6b6f8
TylerStowell <tylerrstowell@gmail.com>
2019-10-24 17:59:24 -0600

    Began autonomous functionality

--- Autobot.java
+++ Autobot.java
@@ -8,7 +8,7 @@ public class Autobot extends OpMode{
   public DcMotor frDrive;
   public DcMotor blDrive;
   public DcMotor brDrive;
-  static final double CIRCUMFERENCE = 12.56637;
+  static final double CIRCUMFERENCE = .1256637;
   static final int INC_PER_METER = 1000;
 
   public void init(){
@@ -24,18 +24,22 @@ public class Autobot extends OpMode{
 
     blDrive.setDirection(DcMotor.Direction.REVERSE);
 
-    //drive(1, 0);
+
   }
 
-  public void loop(){
+  public void start(){
     drive(1, 0);
   }
 
+  public void loop(){
+    
+  }
+
   public void drive(double distance, double angle){
-    double flFinal = 560 * (Math.sqrt(2) * distance / 2 * (Math.sin(angle) + Math.cos(angle))) / (CIRCUMFERENCE * Math.PI);
-    double frFinal = 560 * (Math.sqrt(2) * distance / 2 * (Math.sin(angle) - Math.cos(angle))) / (CIRCUMFERENCE * Math.PI);
-    double blFinal = 560 * (Math.sqrt(2) * distance / 2 * (Math.sin(angle) + Math.cos(angle))) / (CIRCUMFERENCE * Math.PI);
-    double brFinal = 560 * (Math.sqrt(2) * distance / 2 * (Math.sin(angle) - Math.cos(angle))) / (CIRCUMFERENCE * Math.PI);
+    double flFinal = 560 * ((Math.sqrt(2) * distance / 2 * (Math.sin(angle) + Math.cos(angle))) / CIRCUMFERENCE);
+    double frFinal = 560 * ((Math.sqrt(2) * distance / 2 * (Math.sin(angle) - Math.cos(angle))) / CIRCUMFERENCE);
+    double blFinal = 560 * ((Math.sqrt(2) * distance / 2 * (Math.sin(angle) + Math.cos(angle))) / CIRCUMFERENCE);
+    double brFinal = 560 * ((Math.sqrt(2) * distance / 2 * (Math.sin(angle) - Math.cos(angle))) / CIRCUMFERENCE);
     double flPosition = flDrive.getCurrentPosition();
     double frPosition = frDrive.getCurrentPosition();
     double blPosition = blDrive.getCurrentPosition();
@@ -46,21 +50,32 @@ public class Autobot extends OpMode{
       frPosition += frFinal/increments;
       blPosition += blFinal/increments;
       brPosition += brFinal/increments;
-      flDrive.setPower(flPosition - flDrive.getCurrentPosition());
-      frDrive.setPower(frPosition - frDrive.getCurrentPosition());
-      blDrive.setPower(blPosition - blDrive.getCurrentPosition());
-      brDrive.setPower(brPosition - brDrive.getCurrentPosition());
+      telemetry.addData("Final-goal:", flFinal);
+      telemetry.addData("current goal:", flPosition);
+      telemetry.addData("Current:", flDrive.getCurrentPosition());
+      telemetry.addData("power:", flDrive.getPower());
+      telemetry.update();
+      flDrive.setPower(logistic(- flPosition + flDrive.getCurrentPosition()));
+      frDrive.setPower(logistic(- frPosition + frDrive.getCurrentPosition()));
+      blDrive.setPower(logistic(- blPosition + blDrive.getCurrentPosition()));
+      brDrive.setPower(logistic(- brPosition + brDrive.getCurrentPosition()));
     }
+    /*
     while(Math.sqrt(Math.pow(flPosition - flDrive.getCurrentPosition(), 2) + Math.pow(frPosition - frDrive.getCurrentPosition(), 2) + Math.pow(blPosition - blDrive.getCurrentPosition(), 2) + Math.pow(brPosition - brDrive.getCurrentPosition(), 2)) > 100){
       flDrive.setPower(flPosition - flDrive.getCurrentPosition());
       frDrive.setPower(frPosition - frDrive.getCurrentPosition());
       blDrive.setPower(blPosition - blDrive.getCurrentPosition());
       brDrive.setPower(brPosition - brDrive.getCurrentPosition());
     }
+    */
     flDrive.setPower(0);
     frDrive.setPower(0);
     blDrive.setPower(0);
     brDrive.setPower(0);
+  }
+
+  public double logistic(double input){
+    return 2 / (1 + Math.pow(2.71828, -input/1000)) - 1;
   }
 
   public void motorInit(DcMotor m){
