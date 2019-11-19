769ca50a8380053eedd67002ae08768da8cfb31d
Jackson Lucchesi <jacksonlucchesi@RAYMONDs-MacBook-Pro.local>
2019-11-18 18:10:47 -0700

    Made a 1 controller teleop mode to satisfy Vikram

---
+++ Opmodes/Teleop/RemoteControl2.java
@@ -0,0 +1,141 @@
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.*;
+
+/**
+ * @author Jackson and Tyler
+ */
+@TeleOp(name="1 Contoller", group="TeleOp")
+public class RemoteControl2 extends OpMode {
+
+  public DcMotor flDrive;
+  public DcMotor frDrive;
+  public DcMotor blDrive;
+  public DcMotor brDrive;
+  public DcMotor irDrive;
+  public DcMotor ilDrive;
+  public DcMotor outDrive;
+  public NormalizedColorSensor colorSensor;
+  double speed = 1;
+
+  private int outPosition;
+  boolean OutIsClosed = false;
+  boolean clawIsDown;
+  int delay = 0;
+  int delay2 = 0;
+
+  public Intake in;
+  public Outtake out;
+  public MiddlePassage mid;
+  public Claw claw;
+
+  public void init(){
+    flDrive = hardwareMap.get(DcMotor.class, "flDrive");
+    frDrive = hardwareMap.get(DcMotor.class, "frDrive");
+    blDrive = hardwareMap.get(DcMotor.class, "blDrive");
+    brDrive = hardwareMap.get(DcMotor.class, "brDrive");
+    irDrive = hardwareMap.get(DcMotor.class, "irDrive");
+    ilDrive = hardwareMap.get(DcMotor.class, "ilDrive");
+    outDrive = hardwareMap.get(DcMotor.class, "outDrive");
+
+    colorSensor = hardwareMap.get(NormalizedColorSensor.class, "colorSensor");
+    if (colorSensor instanceof SwitchableLight) {
+      ((SwitchableLight)colorSensor).enableLight(true);
+    }
+
+    motorInit(flDrive);
+    motorInit(frDrive);
+    motorInit(blDrive);
+    motorInit(brDrive);
+
+    blDrive.setDirection(DcMotor.Direction.REVERSE);
+    flDrive.setDirection(DcMotor.Direction.REVERSE);
+    irDrive.setDirection(DcMotor.Direction.REVERSE);
+
+    in = new Intake(hardwareMap);
+    out = new Outtake(hardwareMap);
+    mid = new MiddlePassage(hardwareMap);
+    claw = new Claw(hardwareMap);
+
+    outPosition = out.getPosition();
+  }
+
+  public void loop(){
+    if(Math.pow(gamepad1.left_stick_y, 2) + Math.pow(gamepad1.left_stick_x, 2) != 0){
+      flDrive.setPower(speed * (Math.sqrt(2) * gamepad1.left_stick_y / 2 - Math.sqrt(2) * gamepad1.left_stick_x / 2) * Math.min(1 , 1 + gamepad1.right_stick_x));
+      frDrive.setPower(speed * (Math.sqrt(2) * gamepad1.left_stick_y / 2 + Math.sqrt(2) * gamepad1.left_stick_x / 2) * Math.min(1 , 1 - gamepad1.right_stick_x));
+      blDrive.setPower(speed * (Math.sqrt(2) * gamepad1.left_stick_y / 2 + Math.sqrt(2) * gamepad1.left_stick_x / 2)  * Math.min(1 , 1 + gamepad1.right_stick_x));
+      brDrive.setPower(speed * (Math.sqrt(2) * gamepad1.left_stick_y / 2 - Math.sqrt(2) * gamepad1.left_stick_x / 2) * Math.min(1 , 1 - gamepad1.right_stick_x));
+    }
+    if (gamepad1.right_bumper) {
+      in.run();
+      mid.runMotor();
+    }else if(gamepad1.left_bumper){
+      in.reverse();
+      mid.runMotorBack();
+    }else{
+      in.stop();
+      mid.stop();
+    }
+
+    if(gamepad1.left_trigger > 0.1){
+      speed = 0.5;
+    }else{
+      speed = 1;
+    }
+
+    if (gamepad1.right_trigger && delay2 > 25) {
+      if (clawIsDown) {
+        claw.reset();
+        clawIsDown = false;
+      } else {
+        claw.down();
+        clawIsDown = true;
+      }
+      delay2 = 0;
+    }
+
+    if (gamepad1.dpad_up) {
+      out.runToPosition(outPosition + 50);
+    } else if (gamepad1.dpad_down) {
+      out.runToPosition(outPosition - 50);
+    } else {
+      out.stopMotor();
+    }
+    outPosition = out.getPosition();
+
+    if (gamepad1.y) {
+      out.runWheelsOut();
+    } else if (gamepad1.a) {
+      out.runWheelsIn();
+    } else {
+      out.stopWheels();
+    }
+
+    if (gamepad1.b && delay > 25) {
+      if (!OutIsClosed) {
+        out.close();
+        OutIsClosed = true;
+      } else {
+        out.open();
+        OutIsClosed = false;
+      }
+      delay = 0;
+    }
+    delay ++;
+    delay2++;
+
+    if(Math.pow(gamepad1.left_stick_y, 2) + Math.pow(gamepad1.left_stick_x, 2) == 0){
+      flDrive.setPower(-gamepad1.right_stick_x*.6 * speed);
+      frDrive.setPower(gamepad1.right_stick_x*.6 * speed);
+      blDrive.setPower(-gamepad1.right_stick_x*.6 * speed);
+      brDrive.setPower(gamepad1.right_stick_x*.6 * speed);
+    }
+  }
+
+  public void motorInit(DcMotor m){
+    m.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    m.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    m.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+  }
+}
